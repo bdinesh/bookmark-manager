85eec43b45c537dfebddf61a03a766c3a676f25e
Dinesh Bogolu <visitdinu@gmail.com>
2016-11-01 09:41:53 +0530

    Update bookmark service
    - Fixed getBookmarks method

--- src/app/components/bookmark/bookmark.service.js
+++ src/app/components/bookmark/bookmark.service.js
@@ -1,10 +1,10 @@
-function BookmarkService($http, $resource) {
+function BookmarkService($http) {
     let self = this;
 
     self.getBookmarks = function () {
         return $http
             .get('../data/bookmarks.json')
-            .then(response => response.data);
+            .then(response => response.data.items);
     };
 }
 
